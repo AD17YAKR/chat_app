4b7159e959c9ad9514c731e85e3244237a1c3163
AD17YAKR <adityasingh.110601@gmail.com>
2022-08-13 23:39:39 +0530

    Resetting App UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,16 @@
-import 'package:chat_app/utils/color.dart';
-import 'package:chat_app/views/Home_Screen/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:get/get.dart';
-import 'views/Landing_Page/landing_page.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(MyApp());
 }
 
-//292F3F
 class MyApp extends StatefulWidget {
-  MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
@@ -24,109 +19,29 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      //color set to transparent or set your own color
+      statusBarIconBrightness: Brightness.light,
+      //set brightness for icons, like dark background light icons
+    ));
     return ScreenUtilInit(
-      designSize: Size(360, 690),
+      designSize: const Size(360, 690),
       minTextAdapt: true,
       splitScreenMode: true,
-      builder: (context, child) => MaterialApp(
-        debugShowCheckedModeBanner: false,
-        builder: (context, widget) {
-          // ScreenUtil.setContext(context);
-          return MediaQuery(
-            data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
-            child: GetMaterialApp(
-              debugShowCheckedModeBanner: false,
-              builder: (context, child) {
-                return MediaQuery(
-                    data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
-                    child: child!);
-              },
-              home: Home_Screen(),
-              theme: ThemeData(
-                //
-
-                //
-                elevatedButtonTheme: ElevatedButtonThemeData(
-                  style: ElevatedButton.styleFrom(
-                    primary: primaryColor,
-                  ),
-                ),
-                //
-
-                //
-                floatingActionButtonTheme: FloatingActionButtonThemeData(
-                  backgroundColor: primaryColor,
-                ),
-                primaryColor: Colors.grey,
-                //
-
-                //
-                appBarTheme: AppBarTheme(
-                  color: appBarColor,
-                  toolbarTextStyle: TextStyle(color: Colors.white),
-                ),
-                //
-
-                //
-
-                scaffoldBackgroundColor: Colors.white,
-                textTheme: ThemeData.light().textTheme,
-                buttonTheme: ButtonThemeData(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(18.0),
-                  ),
-                  buttonColor: Colors.purple,
-                ),
-              ),
-            ),
-          );
-        },
-      ),
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'First Method',
+          // You can use the library anywhere in the app even in theme
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            textTheme: Typography.englishLike2018.apply(fontSizeFactor: 1.sp),
+          ),
+          home: child,
+        );
+      },
+      child: Container(),
     );
   }
-  /* return ScreenUtilInit(
-    
-      child: GetMaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Home_Screen(),
-
-        //
-        theme: ThemeData(
-          //
-
-          //
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-              primary: primaryColor,
-            ),
-          ),
-          //
-
-          //
-          floatingActionButtonTheme: FloatingActionButtonThemeData(
-            backgroundColor: primaryColor,
-          ),
-          primaryColor: Colors.grey,
-          //
-
-          //
-          appBarTheme: AppBarTheme(
-            color: appBarColor,
-            toolbarTextStyle: TextStyle(color: Colors.white),
-          ),
-          //
-
-          //
-
-          scaffoldBackgroundColor: Colors.white,
-          textTheme: ThemeData.light().textTheme,
-          buttonTheme: ButtonThemeData(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(18.0),
-            ),
-            buttonColor: Colors.purple,
-          ),
-        ),
-      ),
-    ); */
 }
--- lib/utils/color.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color appBarColor = Color(0xff075e54);
-Color primaryColor = Color(0xff075e54);
-Color secondaryColor = Color(0xff128C7e);
-
-String imageUrl =
-    "https://images.unsplash.com/photo-1656603758843-c59817dba6ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60";
